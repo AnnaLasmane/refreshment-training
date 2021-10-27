fbef39f16021981ce0c7c7da8f0afaddb49131f5
Anna Lasmane <anna.lasmane@accenture.com>
2021-10-27 10:44:15 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const image = document.querySelector(".js-image")
+const show = document.querySelector(".js-show")
+const hide = document.querySelector(".js-hide")
+
+hide.addEventListener("click", () => {
+    image.classList.add("hidden")
+});
+
+show.addEventListener("click", () => {
+image.classList.remove('hidden')
+});
+
